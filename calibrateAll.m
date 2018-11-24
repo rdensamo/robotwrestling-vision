9bd4714893c4e5ef9e40594dadafb8b3810f9177
rdensamo <rbd218@lehigh.edu>
2018-11-23 23:47:20 -0500

    calibrating 4 colors

--- calibrateAll.m
+++ calibrateAll.m
@@ -3,7 +3,7 @@ mypath = '/home/vader/capstonedata/noexp2.bag';
 flat_points = ringCalibrate(mypath, 1);
 [thresh_l, thresh_r, pcxyz] = colorCalibrate(mypath, 1);
 %pause(20); 
-[rob_x_pos, rob_y_pos, rob_theta] = trackRobot(mypath, thresh_l, thresh_r, 1);
+%[rob_x_pos, rob_y_pos, rob_theta] = trackRobot(mypath, thresh_l, thresh_r, 1);
 
 
 
--- colorCalibrate.m
+++ colorCalibrate.m
@@ -1,4 +1,4 @@
-function [hsv_thresh_l, hsv_thresh_r, pcxyz] = colorCalibrate(filepath, option)
+function [hsv_thresh_l1, hsv_thresh_r1, hsv_thresh_l2, hsv_thresh_r2,pcxyz] = colorCalibrate(filepath, option)
 % TODO: Fix this function comment 
 % TODO: ONCE YOU WRITE ROS SUBSCRIBER SWITCH BAG PARAMETER WITH TOPIC
 %colorCalibrate gittakes bag file path and opion argument for graphing
@@ -35,15 +35,25 @@ hsv_pts = rgb2hsv(top_img);
 figure; 
 imagesc(hsv_pts); 
 % Allows you to select the polygone area with colors
-mask_l = roipoly; % select left target 
-mask_r = roipoly; % select right target
+mask_r1_l = roipoly; % select left target robot 1
+mask_r1_r = roipoly; % select right target robot 1 
+
+mask_r2_l = roipoly; % select left target robot 2
+mask_r2_r = roipoly; % select right target robot 2 
 
 if option == 1
     % Visualize selected region
+    % robot 1 
     figure; 
-    imagesc(mask_l);
+    imagesc(mask_r1_l);
     figure; 
-    imagesc(mask_r);
+    imagesc(mask_r1_r);
+    
+    % robot 2 
+    figure; 
+    imagesc(mask_r2_l);
+    figure; 
+    imagesc(mask_r2_r);
 end
 
 
@@ -53,39 +63,78 @@ pcxyz = pcxyz(~bad,:);
 x_cords = pcxyz(:,1); 
 y_cords = pcxyz(:,2); 
 
-% left target
-[hsv_thresh_l] = getThresh(hsv_pts, mask_l, init_thresh);
-[bin_l] = seg_color(hsv_pts, hsv_thresh_l);
-[x_target_l, y_target_l] = gettarget(bin_l);
-x_target_l = round(x_target_l); 
-y_target_l = round(y_target_l);
-x_real_l = x_cords(x_target_l); 
-y_real_l = y_cords(y_target_l);
+%robot1: 
+% left target robot 1 
+[hsv_thresh_l1] = getThresh(hsv_pts, mask_r1_l , init_thresh);
+[bin_l1] = seg_color(hsv_pts, hsv_thresh_l1);
+[x_target_l1, y_target_l1] = gettarget(bin_l1);
+x_target_l1 = round(x_target_l1); 
+y_target_l1 = round(y_target_l1);
+x_real_l1 = x_cords(x_target_l1); 
+y_real_l1 = y_cords(y_target_l1);
 
-% right target 
-[hsv_thresh_r] = getThresh(hsv_pts, mask_r, init_thresh);
-[bin_r] = seg_color(hsv_pts, hsv_thresh_r); 
-[x_target_r, y_target_r] = gettarget(bin_r); 
-x_target_r = round(x_target_r);
-y_target_r = round(y_target_r); 
-x_real_r = x_cords(x_target_r); 
-y_real_r = y_cords(y_target_r);
-x_reals = [x_real_l, x_real_r];
-y_reals = [y_real_l, y_real_r]; 
-rob_x_pos = mean(x_reals); 
-rob_y_pos = mean(y_reals); 
+% right target robot 1 
+[hsv_thresh_r1] = getThresh(hsv_pts, mask_r1_r, init_thresh);
+[bin_r1] = seg_color(hsv_pts, hsv_thresh_r1); 
+[x_target_r1, y_target_r1] = gettarget(bin_r1); 
+x_target_r1 = round(x_target_r1);
+y_target_r1 = round(y_target_r1); 
+x_real_r1 = x_cords(x_target_r1); 
+y_real_r1 = y_cords(y_target_r1);
+x_reals1 = [x_real_l1, x_real_r1];
+y_reals1 = [y_real_l1, y_real_r1]; 
+rob_x_pos1 = mean(x_reals1); 
+rob_y_pos1 = mean(y_reals1); 
 
-% Must ensure -pi to pi 
-rob_theta = atan2(rob_y_pos, rob_x_pos);
+% robot 2:
+% left target robot 2
+[hsv_thresh_l2] = getThresh(hsv_pts, mask_r2_l , init_thresh);
+[bin_l2] = seg_color(hsv_pts, hsv_thresh_l2);
+[x_target_l2, y_target_l2] = gettarget(bin_l2);
+x_target_l2 = round(x_target_l2); 
+y_target_l2 = round(y_target_l2);
+x_real_l2 = x_cords(x_target_l2); 
+y_real_l2 = y_cords(y_target_l2);
+
+% right target robot 2 
+[hsv_thresh_r2] = getThresh(hsv_pts, mask_r2_r , init_thresh);
+[bin_r2] = seg_color(hsv_pts, hsv_thresh_r2); 
+[x_target_r2, y_target_r2] = gettarget(bin_r2); 
+x_target_r2 = round(x_target_r2);
+y_target_r2 = round(y_target_r2); 
+x_real_r2 = x_cords(x_target_r2); 
+y_real_r2 = y_cords(y_target_r2);
+x_reals2 = [x_real_l2, x_real_r2];
+y_reals2 = [y_real_l2, y_real_r2]; 
+rob_x_pos2 = mean(x_reals2); 
+rob_y_pos2 = mean(y_reals2); 
+
+% Robot 1 
+% Must ensure -pi to pi r 
+rob_theta1 = atan2(rob_y_pos1, rob_x_pos1);
 disp("original rob_theta: "); 
-disp(rob_theta); 
+disp(rob_theta1); 
 % Want to rotate so bumpers are at pi (180 degrees)
 % use what Prof. Spletzer talked to Jerett about doing rotation
 % want to rotate by 90 degrees 
-rob_theta = rob_theta + (pi/2); 
-rob_theta = atan2(sin(rob_theta),cos(rob_theta)); 
+rob_theta1 = rob_theta1 + (pi/2); 
+rob_theta1 = atan2(sin(rob_theta1),cos(rob_theta1)); 
 disp("rotated rob_theta: "); 
-disp(rob_theta); 
+disp(rob_theta1); 
+
+% Robot 2
+% Must ensure -pi to pi r 
+rob_theta2 = atan2(rob_y_pos2, rob_x_pos2);
+disp("original rob_theta: "); 
+disp(rob_theta2); 
+% Want to rotate so bumpers are at pi (180 degrees)
+% use what Prof. Spletzer talked to Jerett about doing rotation
+% want to rotate by 90 degrees 
+rob_theta2 = rob_theta2 + (pi/2); 
+rob_theta2 = atan2(sin(rob_theta2),cos(rob_theta2)); 
+disp("rotated rob_theta: "); 
+disp(rob_theta2); 
+
 
 
 
@@ -95,9 +144,14 @@ if option == 1
          figure 
             imagesc(top_img);
         hold on; 
-     
-        h_l = plot(x_target_l, y_target_l,'b+', 'markersize', 20,'linewidth',2);  
-        h_r = plot(x_target_r, y_target_r,'r+', 'markersize', 20,'linewidth',2);  
+        %robot 1 
+        h_l1 = plot(x_target_l1, y_target_l1,'b+', 'markersize', 20,'linewidth',2);  
+        h_r1 = plot(x_target_r1, y_target_r1,'r+', 'markersize', 20,'linewidth',2);  
+        
+        %robot 2 
+        h_l2 = plot(x_target_l2, y_target_l2,'g+', 'markersize', 20,'linewidth',2);  
+        h_r2 = plot(x_target_r2, y_target_r2,'y+', 'markersize', 20,'linewidth',2);  
+        
 end 
 
 end
